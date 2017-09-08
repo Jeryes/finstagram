e5c455dc64689e6a03f4b276f5ae1785d767fea3
Jean Escalante <jean@eastsidegamestudio.com>
2017-09-08 03:15:12 +0000

    added new post page, now sharing styling across all pages, and each post has their own page

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @posts = Post.order(created_at: :desc)
     erb :index
@@ -21,8 +27,64 @@ post '/signup' do
     
     # if user validations pass and user is saved
     if @user.save
-        "User #{username} saved!"
+        redirect to('/login')
     else
         erb(:signup)
     end
+    
 end
+    
+get '/login' do     # when a GET request comes into /login
+        erb(:login) # render app/views/login.erb
+end
+
+post '/login' do    # when we submit a form with an action of /login
+   username = params[:username]
+   password = params[:password]
+   
+   # 1. find user by username
+   user = User.find_by(username: username)
+   
+   # 2. if that user exists
+   if user && user.password == password
+       session[:user_id] = user.id
+       redirect to('/')
+   else
+       @error_message = "Login failed."
+       erb(:login)
+   end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
+get '/posts/new' do
+    @post = Post.new
+    erb(:"posts/new")
+end
+
+post '/posts' do
+    photo_url = params[:photo_url]
+    
+    # instantiate new Post
+    @post = Post.new({ photo_url: photo_url, user_id: current_user
+    .id })
+    
+    # if @post validates, save
+    if @post.save
+        redirect(to('/'))
+    else
+        erb(:"posts/new")
+    end
+end
+
+get '/posts/:id' do
+    @post = Post.find(params[:id])  # find the post with the ID from the URL
+    erb(:"posts/show")       # print to the screen for now
+end
+
+# delete '/users/:id/comments/:comment_id' do 
+# =>    
+# end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,17 @@
 class Post < ActiveRecord::Base
     
+    # (this is where you associations are e.g. has_many :posts, etc.)...
+    
     belongs_to :user
     has_many :comments
     has_many :likes
     
+    # validations in between association definiations and methods!
+    
+    validates_presence_of :photo_url, :user
+    
+    # (this is where your def humanized_time_ago method is, along with the rest of your methods in this file)...
+        
     def humanized_time_ago
         time_ago_in_seconds = Time.now - self.created_at
         time_ago_in_minutes = time_ago_in_seconds / 60
@@ -18,7 +26,7 @@ class Post < ActiveRecord::Base
         elsif time_ago_in_minutes >= 60
             "#{(time_ago_in_minutes / 60).to_i} hours ago" 
         else
-            "#{time_ago_in_minuates.to_i} minutes ago"
+            "#{time_ago_in_minutes.to_i} minutes ago"
         end
     end
     
